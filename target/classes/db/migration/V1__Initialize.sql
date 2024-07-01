6ae1cc014c27a4ee6f46aeffb7866570e8ca3615
senri.arai <senri.arai@persol.co.jp>
2024-07-01 15:56:06 +0900

    不要なtargetフォルダを削除

--- target/classes/db/migration/V1__Initialize.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS sample_person (
-    id int,
-    name varchar(50),
-    age int
-);
--- target/classes/db/migration/V2__initInsert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO todo VALUES (1, "タスク")
--- (1,'ANTHONY',10)
--- (2,'BECKY',20),
--- (3,'CAROL',30);
